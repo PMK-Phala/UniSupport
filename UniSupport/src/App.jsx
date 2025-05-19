b86d759f478427064bee611bf14abfe33f14c958
Phalafala Phala <khumo.m.phala@gmail.com>
2025-05-19 16:38:25 +0200

    initial commit

---
+++ UniSupport/src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+
+        const containerStyle = {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height:"100vh"
+        };
+
+    return(
+        <h1 style={ containerStyle}>UniSupport Comming soon</h1>
+    )
+}
+export default App
